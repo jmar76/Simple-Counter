a13313ab35c093ccf86a58dc7153155efa79bfa4
jmar76 <jmaguero.ing@gmail.com>
2021-05-03 13:48:35 +0000

    ejrcicios terminado, entiendo que esto e slo que pide

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "../../styles/home.scss";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+	ReactDOM.render(
+		<Home
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
 	);
-}
+}, 1000);
+export const Home = props => {
+	return (
+		<Fragment>
+			<div className="Contador">
+				<div className="Calendario">
+					<i className="fas fa-clock"></i>
+				</div>
+				<div className="four">{props.digitFour % 10}</div>
+				<div className="three">{props.digitThree % 10}</div>
+				<div className="two">{props.digitTwo % 10}</div>
+				<div className="one">{props.digitOne % 10}</div>
+			</div>
+		</Fragment>
+	);
+};
+Home.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
